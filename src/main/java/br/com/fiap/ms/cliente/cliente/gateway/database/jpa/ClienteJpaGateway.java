cf12c05bc71c8ceeb774c39af25164e17c6031d5
dsouzajm <danielsouza.jm@gmail.com>
2025-07-06 19:43:18 -0300

    Correcao para funcionamento correto do PUT.

--- src/main/java/br/com/fiap/ms/cliente/cliente/gateway/database/jpa/ClienteJpaGateway.java
+++ src/main/java/br/com/fiap/ms/cliente/cliente/gateway/database/jpa/ClienteJpaGateway.java
@@ -11,9 +11,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import static br.com.fiap.ms.cliente.cliente.utils.ClienteUtils.convertToCliente;
+import static br.com.fiap.ms.cliente.cliente.utils.ClienteUtils.convertToClienteEntity;
 
 @AllArgsConstructor
 @Component
@@ -24,7 +28,8 @@ public class ClienteJpaGateway implements ClienteGateway {
     @Override
     public Optional<Cliente> buscarClientePorCpf(String cpf) {
         ClienteEntity clienteEntity = clienteRepository.findByCpf(cpf).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-        return Optional.ofNullable(convertToCliente(clienteEntity));
+        Optional<Cliente> cliente = Optional.ofNullable(convertToCliente(clienteEntity));
+        return cliente;
     }
 
     @Override
@@ -43,7 +48,20 @@ public class ClienteJpaGateway implements ClienteGateway {
     @Override
     public Optional<Cliente> alterarClientePorCpf(String cpf, Cliente cliente) {
         ClienteEntity clienteEntity = clienteRepository.findByCpf(cpf).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        Optional<Cliente> cliente2 = Optional.ofNullable(convertToCliente(clienteEntity));
         clienteEntity.setCpf(cpf);
+
+        clienteEntity.setNome(cliente.getNome());
+        clienteEntity.setDataNascimento(cliente.getDataNascimento());
+        clienteEntity.getEndereco().setId(cliente.getEndereco().getId());
+        clienteEntity.getEndereco().setRua(cliente.getEndereco().getRua());
+        clienteEntity.getEndereco().setNumero(cliente.getEndereco().getNumero());
+        clienteEntity.getEndereco().setCep(cliente.getEndereco().getCep());
+        clienteEntity.getEndereco().setComplemento(cliente.getEndereco().getComplemento());
+        clienteEntity.getEndereco().setBairro(cliente.getEndereco().getBairro());
+        clienteEntity.getEndereco().setCidade(cliente.getEndereco().getCidade());
+        clienteEntity.setDataUltimaAlteracao(LocalDateTime.now());
+
         ClienteEntity clienteAtualizado = clienteRepository.save(clienteEntity);
         return Optional.ofNullable(ClienteUtils.convertToCliente(clienteAtualizado));
     }
