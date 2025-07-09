cfa3982f0501e215321b6250226dbbb19c4fb2be
dsouzajm <danielsouza.jm@gmail.com>
2025-07-09 20:46:20 -0300

    Retirada de controller e usecases relacionadas a endereco, correcao de metodos utils.

--- src/main/java/br/com/fiap/ms/cliente/cliente/usecase/ExcluirClientePorCpfUseCase.java
+++ src/main/java/br/com/fiap/ms/cliente/cliente/usecase/ExcluirClientePorCpfUseCase.java
@@ -1,6 +1,5 @@
 package br.com.fiap.ms.cliente.cliente.usecase;
 
-import br.com.fiap.ms.cliente.cliente.domain.Cliente;
 import br.com.fiap.ms.cliente.cliente.gateway.ClienteGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
