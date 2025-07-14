aebe5559c15ad2dcc73fdc388499984f304124a3
dsouzajm <danielsouza.jm@gmail.com>
2025-07-13 21:36:09 -0300

    Inclusao de h2 e correcao para testes unitarios.

--- src/test/java/br/com/fiap/shopsphere/ms/cliente/ClienteApplicationTests.java
+++ src/test/java/br/com/fiap/shopsphere/ms/cliente/ClienteApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.fiap.shopsphere.ms.cliente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClienteApplicationTests {
 
 	@Test
